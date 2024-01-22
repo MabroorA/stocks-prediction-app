49e6efc0114ac115ff947a93ce4777252aba84f5
Mabroor Ahmed <mabroorahmed18@gmail.com>
2024-01-22 01:53:32 +0000

    Menu start

---
+++ finance-assistance-tool/src/Menu_bar.tsx
@@ -0,0 +1,27 @@
+function Menu_bar(){
+    return (
+    <div className="menu_bar" >
+        <h1>Menu bar</h1>
+        <ul>
+            <li>
+                Daily View
+            </li>
+            <li>
+                My Profortlio
+            </li>
+            <li>
+                My account 
+            </li>
+            <li>
+                Search bar
+            </li>
+
+        
+        </ul> 
+    
+    </div>
+
+    )
+}
+
+export default Menu_bar
--- finance-assistance-tool/src/main.tsx
+++ finance-assistance-tool/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Menu_bar from "./Menu_bar.tsx"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Menu_bar />
     <App />
   </React.StrictMode>,
 )
