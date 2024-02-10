327dda3dbbeda4f64b1b4999affaee52f0b7004b
Mabroor Ahmed <mabroorahmed18@gmail.com>
2024-02-10 19:36:51 +0000

    Daily data Api call setup in backend

--- backend-fct/src/data-fetching/fetch-stocks-data.ts
+++ backend-fct/src/data-fetching/fetch-stocks-data.ts
@@ -1,7 +1,8 @@
-export async function fetchStocksData() {
+export async function daily_fetchStocksData() {
   const API_KEY = process.env.API_KEY;
   const request = await fetch(
-    "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
+    "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=" +
+      { API_KEY }
   );
   const response = await request.json();
   return response;
--- backend-fct/src/index.ts
+++ backend-fct/src/index.ts
@@ -1,11 +1,20 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { fetchStocksData } from "./data-fetching/fetch-stocks-data";
+import { daily_fetchStocksData } from "./data-fetching/fetch-stocks-data";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT ?? 3000;
+const cors = require("cors");
+
+// cors access
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   console.log("WE JUST GOT A REQUEST AT THIS ENDPOINT");
@@ -15,8 +24,8 @@ app.get("/", (req: Request, res: Response) => {
 app.get("/fetch-data", async (req: Request, res: Response) => {
   console.log("RECEIVED A CALL AT FETCH DATA ENDPOINT");
   try {
-    console.log("making a call to the external API");
-    const response = await fetchStocksData();
+    console.log("Calling external API (alpha vantage) ");
+    const response = await daily_fetchStocksData();
     res.send(response);
   } catch (error) {
     console.log(error);
@@ -24,6 +33,18 @@ app.get("/fetch-data", async (req: Request, res: Response) => {
   }
 });
 
+// Daily api call from alphavantage
+app.get("/daily-view", async (req: Request, res: Response) => {
+  console.log("RECEIVED A CALL AT FETCH DATA ENDPOINT");
+  try {
+    console.log("Calling external API (alpha vantage) ");
+    const response = await daily_fetchStocksData();
+    res.send(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Failed to get data");
+  }
+});
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
