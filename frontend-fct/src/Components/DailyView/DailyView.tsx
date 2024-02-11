6303c50f090c817f8cc963c6101eb9aaaa6ab6d2
Mabroor Ahmed <mabroorahmed18@gmail.com>
2024-02-11 00:34:41 +0000

    Daily Table View completed using api data

--- frontend-fct/src/Components/DailyView/DailyView.tsx
+++ frontend-fct/src/Components/DailyView/DailyView.tsx
@@ -1,6 +1,34 @@
 import { useEffect, useState } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  CartesianGrid,
+  YAxis,
+  Tooltip,
+  Legend,
+} from "recharts";
 import "./DailyView.css";
 function DailyView() {
+  // Define the type for the fetched data
+  interface DailyData {
+    datetime: string;
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    volume: number;
+    avgprice: number;
+    meta: {
+      symbol: string;
+      interval: string;
+      currency: string;
+      exchange_timezone: string;
+      exchange: string;
+      mic_code: string;
+      type: string;
+    };
+  }
   // getting data from server api call
   async function fetchServerData() {
     const serverRequest = await fetch("http://localhost:3000/daily-view");
@@ -9,21 +37,38 @@ function DailyView() {
     return response;
   }
 
-  const [data, setData] = useState(null); // State to store the fetched data
+  const [data, setData] = useState<DailyData[]>([]); // State to store the fetched data
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetchServerData();
-      setData(response); // Set the fetched data in state
+      setData(response.values); // Set the fetched data in state
       console.log(response, "Daily View Working");
     };
     fetchData();
   }, []);
 
+  // line graph
+
   return (
     <>
       <div className="daily-view">
-        <div>DailyView Data </div>;
+        <div>DailyView Data </div>
+        {data.length > 0 && (
+          <LineChart
+            width={800}
+            height={400}
+            data={data}
+            margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="datetime" />
+            <YAxis dataKey="avgprice" />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="avgprice" stroke="#8884d8" />
+          </LineChart>
+        )}
         {data && (
           <div>
             <h2>json data from server :</h2>
