5009fd86b52327f1154cc5e4c60dcc411d923137
Mabroor Ahmed <ma942@student.le.ac.uk>
2024-03-04 00:08:14 +0000

    button to toggle between top gainers and losers

--- frontend-fct/src/Pages/Home/HomePage.tsx
+++ frontend-fct/src/Pages/Home/HomePage.tsx
@@ -2,10 +2,18 @@ import "./HomePage.css";
 import NavBar from "../../Components/Navbar/NavBar";
 import Bottom_5 from "../../Components/Bottom-5/Bottom_5";
 import Top_5 from "../../Components/Top-5/Top_5";
+import { useState } from "react";
 // import MovingAverageView from "../../Components/MovingAverageView/MovingAverageView";
 // import Top_5 from "../../Components/Top-5/Top_5";
 
 export default function HomePage() {
+
+  const [showTop5, setShowTop5] = useState(true);
+
+  const toggleSection = () => {
+    setShowTop5(!showTop5);
+  };
+  
   return (
     <>
       <NavBar />
@@ -22,10 +30,22 @@ export default function HomePage() {
           <button className="predict-button">Start Predicting</button>
         </div>
         <div className="right-container">
-          <h1 className="title-stock">Top 5 Gainers</h1>
+          <div className="right-container-header">
+            <div className="toggle-button-container">
+                <button className="toggle-button" onClick={toggleSection}>
+                  {showTop5 ? "Show Bottom 5" : "Show Top 5"}
+                </button>
+            </div>
+            <div>
+              <h1 className="title-stock">
+                {showTop5 ? "Top 5 Gainers" : "Bottom 5 Losers"}
+              </h1>
+            </div>  
+          </div>    
           <div className="top-stocks">
-            <Top_5/>
+            {showTop5 ? <Top_5 /> : <Bottom_5 />}
           </div>
+          
         </div>
       </div>
       <div className="bottom-container">
