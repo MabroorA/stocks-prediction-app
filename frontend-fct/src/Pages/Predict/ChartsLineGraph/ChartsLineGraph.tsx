35916bd99ce7b4e290db46ee94752178132a49ef
Mabroor Ahmed <mabroorahmed18@gmail.com>
2024-03-29 01:53:31 +0000

    unused import removed

--- frontend-fct/src/Pages/Predict/ChartsLineGraph/ChartsLineGraph.tsx
+++ frontend-fct/src/Pages/Predict/ChartsLineGraph/ChartsLineGraph.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Chart as ChartJS, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, 
 Legend, Filler} from "chart.js"
 import { Line } from "react-chartjs-2";
