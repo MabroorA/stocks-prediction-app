572f63013d69705c637f533ff2a0e26e23aa3c2a
Mabroor Ahmed <ma942@student.le.ac.uk>
2024-04-24 18:07:51 +0100

    search bar added with icon

--- frontend-fct/src/Components/Navbar/NavBar.tsx
+++ frontend-fct/src/Components/Navbar/NavBar.tsx
@@ -1,4 +1,6 @@
 import "./NavBar.css";
+import { CiSearch } from "react-icons/ci";
+
 
 export default function NavBar() {
   return (
@@ -7,11 +9,16 @@ export default function NavBar() {
         <a className="Logo" href="/">
           Home
         </a>
-        <a href="/analyse">Learn</a>
 
-        <a href="/search">Search Stocks</a>
+        <div className="search-container">
+          <input className="menu-search-bar" type="text" placeholder=" Search..." />
+          <div className="search-icon">
+            <CiSearch />
+          </div>
+        </div>
       </div>
       <div>
+        <a href="/analyse">Blog</a>
         <a href="/predict">Predict</a>
         <a href="/news">News</a>
       </div>
