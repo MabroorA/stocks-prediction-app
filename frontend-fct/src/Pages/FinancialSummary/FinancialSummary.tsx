a20d5d497b4e4ecf7d49c55955c62974745d4c8d
ma942 <ma942@student.le.ac.uk>
2024-04-25 23:18:29 +0100

    api call added from server

--- frontend-fct/src/Pages/FinancialSummary/FinancialSummary.tsx
+++ frontend-fct/src/Pages/FinancialSummary/FinancialSummary.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "../../Components/Navbar/NavBar"
 import { StockSummary } from "../../types";
 import "./FinancialSummary.css"
@@ -41,117 +41,126 @@ const mockIBMSummary: StockSummary = {
 export default function FinancialSummary() {
   
   // saving stock summary  
-  const [stockSummary, setstockSummary] = useState<StockSummary>(mockIBMSummary);
+  const [stockSummary, setStockSummary] = useState<StockSummary>(mockIBMSummary);
+
+  useEffect(() => {
+    const fetchStockData = async () => {
+      const response = await fetch("http://localhost:3000/financial-summary?ticker=AAPL");
+      const data = await response.json();
+      setStockSummary(data[0]); // Assuming the API response is an array with one object
+    };
+
+    fetchStockData();
+  }, []);
 
 
-  return (
-    <>
-      <NavBar />
-      <div className="financial-summary">
-        <div className="financial-summary-banner">
-          <div className="banner-left">
-            <img className="logo-img" src={ibmImage} alt="IBM Logo" />
-          </div>
 
-          <div className="banner-right">
-            <div className="stock-name-info">
-              <h1 className="stock-full-name">{stockSummary.companyName}</h1>
-              <h1 className="stock-ticker">{stockSummary.symbol}</h1>
-            </div>
-            <div className="stock-price-info">
-              <h1 className="stock-price">{stockSummary.price.toFixed(2)}</h1> {/* Format price with toFixed */}
-              <h1 className="stock-price-change">
-                {stockSummary.changes}({(stockSummary.changes / stockSummary.price * 100).toFixed(2)}%)
-              </h1>
-            </div>
-          </div>
+
+return (
+  <>
+    <NavBar />
+    <div className="financial-summary">
+      <div className="financial-summary-banner">
+        <div className="banner-left">
+          <img className="logo-img" src={stockSummary.image} alt={stockSummary.companyName + " Logo"} />
         </div>
 
-        <div className="stock-sections">
-          <div className="stocks-left-sections">
-            <div className="stock-about-section">
-              <h1>About</h1>
-              <div className="about-sections">
-                  <div>
-                    <div className="stock-about-headers">
-                        <div className="about-header-section">
-                          <h2>CEO</h2>
-                          <p>{stockSummary.ceo}</p>
-                        </div>
-                        <div className="about-header-section">
-                          <h2>Sector</h2>
-                          <p>{stockSummary.sector}</p>
-                        </div>
-                        <div className="about-header-section">
-                          <h2>Industry</h2>
-                          <p>{stockSummary.industry}</p>
-                        </div>
-                        <div className="about-header-section">
-                          <h2>Website</h2>
-                          <a href={stockSummary.website} title="{stockSummary.website}" >visit</a>
-                        </div>
-                        <div className="about-header-section">
-                          <h2>Exchange</h2>
-                          <p>{stockSummary.exchange}</p>
-                        </div>
-                    </div>
-
-                    <div className="about-description">
-                      <h1>Description</h1>
-                      <p>{stockSummary.description}</p>
-                    </div>
-                  </div>
-              
-
-                  <div className="about-sidebar">
-                      <div className="about-sidebar-sections">
-                        <div className="about-sidebar-section">
-                          <h3>IPO Date</h3>
-                          <p>{mockIBMSummary.ipoDate}</p>
-                        </div>
-                        <div className="about-sidebar-section">
-                          <h3>Employee</h3>
-                          <p>{mockIBMSummary.fullTimeEmployees}</p>
-                          
-                        </div>
-                        <div className="about-sidebar-section">
-                          <h3>Country</h3>
-                          <p>{mockIBMSummary.country}</p>
-                        </div>
-                      </div>
-                  </div>
-
-                </div>
-            </div>
-
-            <div className="stocks-left-half-blocks">
-              <div className="stock-summary-section">
-                <h1>Summary</h1>
-                <h3>2 rows with important Information</h3> {/* Placeholder for now */}
-              </div>
-              <div className="stock-historical-prices-section">
-                <h1>Historical Prices</h1>
-                <h3>A graph will go here</h3> {/* Placeholder */}
-              </div>
-            </div>
+        <div className="banner-right">
+          <div className="stock-name-info">
+            <h1 className="stock-full-name">{stockSummary.companyName}</h1>
+            <h1 className="stock-ticker">{stockSummary.symbol}</h1>
           </div>
-
-          <div className="stock-news-section">
-            <h1>Latest {mockIBMSummary.symbol} News</h1>
-            <div className="news-sections">
-              <div className="news-card">
-                <img className="logo-img" src={ibmImage} alt="IBM Logo" />
-                <div className="news-card-text">
-                <h3>News title(IBM CAME OUT WITH NEW GPU) </h3>
-                <p style={{color:"blue"}}>5 minutes ago</p>
-                </div>
-              </div>
-
-            </div>
-
+          <div className="stock-price-info">
+            <h1 className="stock-price">{stockSummary.price.toFixed(2)}</h1> {/* Format price with toFixed */}
+            <h1 className="stock-price-change">
+              {stockSummary.changes}({(stockSummary.changes / stockSummary.price * 100).toFixed(2)}%)
+            </h1>
           </div>
         </div>
       </div>
-    </>
-  );
+
+      <div className="stock-sections">
+        <div className="stocks-left-sections">
+          <div className="stock-about-section">
+            <h1>About</h1>
+            <div className="about-sections">
+              <div>
+                <div className="stock-about-headers">
+                  <div className="about-header-section">
+                    <h2>CEO</h2>
+                    <p>{stockSummary.ceo}</p>
+                  </div>
+                  <div className="about-header-section">
+                    <h2>Sector</h2>
+                    <p>{stockSummary.sector}</p>
+                  </div>
+                  <div className="about-header-section">
+                    <h2>Industry</h2>
+                    <p>{stockSummary.industry}</p>
+                  </div>
+                  <div className="about-header-section">
+                    <h2>Website</h2>
+                    <a href={stockSummary.website} title="{stockSummary.website}" >visit</a>
+                  </div>
+                  <div className="about-header-section">
+                    <h2>Exchange</h2>
+                    <p>{stockSummary.exchange}</p>
+                  </div>
+                </div>
+
+                <div className="about-description">
+                  <h1>Description</h1>
+                  <p>{stockSummary.description}</p>
+                </div>
+              </div>
+
+              <div className="about-sidebar">
+                <div className="about-sidebar-sections">
+                  <div className="about-sidebar-section">
+                    <h3>IPO Date</h3>
+                    <p>{stockSummary.ipoDate}</p>
+                  </div>
+                  <div className="about-sidebar-section">
+                    <h3>Employee</h3>
+                    <p>{stockSummary.fullTimeEmployees}</p>
+                  </div>
+                  <div className="about-sidebar-section">
+                    <h3>Country</h3>
+                    <p>{stockSummary.country}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="stocks-left-half-blocks">
+            <div className="stock-summary-section">
+              <h1>Summary</h1>
+              <h3>2 rows with important Information from the API response</h3> {/* Placeholder for now */}
+            </div>
+            <div className="stock-historical-prices-section">
+              <h1>Historical Prices</h1>
+              <h3>A graph will go here</h3> {/* Placeholder */}
+            </div>
+          </div>
+        </div>
+
+        <div className="stock-news-section">
+          <h1>Latest {mockIBMSummary.symbol} News</h1>
+          <div className="news-sections">
+            <div className="news-card">
+              <img className="logo-img" src={ibmImage} alt="IBM Logo" />
+              <div className="news-card-text">
+                <h3>News title(IBM CAME OUT WITH NEW GPU) </h3>
+                <p style={{ color: "blue" }}>5 minutes ago</p>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </>
+);
 }
