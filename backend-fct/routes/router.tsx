7823b595b72402719a6e77c0b3eae51a9173d723
ma942 <ma942@student.le.ac.uk>
2024-04-25 22:29:01 +0100

    ALL unused functions removed

--- backend-fct/routes/router.tsx
+++ backend-fct/routes/router.tsx
@@ -1,15 +1,9 @@
 import {Router ,Request, Response} from "express";
 import {
-  Exponential_Moving_Avg,
-  Grouped_Daily,
-  Historical_Daily_By_Ticker,
   IntraDay,
-  IntraDay_Given_Stock_and_Timeframe,
   Losers,
   News_latest,
   Search_ticker,
-  Search_to_display_ticker,
-  Single_Stock_Qoute,
   Stock_Company_Profile,
   top5,
 } from "../src/data-fetching/fetch-stocks-data";
@@ -24,42 +18,7 @@ router.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
   });
   
-// router.get("/fetch-data", async (req: Request, res: Response) => {
-// console.log("RECEIVED A CALL AT FETCH DATA ENDPOINT");
-// try {
-//     console.log("Calling external API (alpha vantage) ");
-//     const response = await daily_fetchStocksData();
-//     res.send(response);
-// } catch (error) {
-//     console.log(error);
-//     res.status(500).send("Failed to get data");
-// }
-// });
-  
-// Daily api call from alphavantage
-router.get("/daily-view", async (req: Request, res: Response) => {
-console.log("RECEIVED A CALL AT FETCH DATA ENDPOINT");
-try {
-    console.log("12DATA API called  ");
-    const response = await Exponential_Moving_Avg();
-    res.send(response);
-} catch (error) {
-    console.log(error);
-    res.status(500).send("Failed to get data");
-}
-});
 
-// Exponential_Moving_Avg api call from polygen
-router.get("/exponential-moving-avg", async (req: Request, res: Response) => {
-try {
-    console.log("Ploygen API called  ");
-    const response = await Exponential_Moving_Avg();
-    res.send(response);
-} catch (error) {
-    console.log(error);
-    res.status(500).send("Failed to get data");
-}
-});
 // Searching a ticker
 // Route to handle ticker search
 router.get("/search-ticker", async (req: Request, res: Response) => {
@@ -76,33 +35,7 @@ try {
 }
 });
 
-// Searching to display 
-// Searching to display
-router.get("/ticker-to-display", async (req: Request, res: Response) => {
-try {
-    const { ticker } = req.query; // Get the ticker parameter from the request query
-    if (!ticker || typeof ticker !== "string") {
-        throw new Error("Ticker parameter is missing or invalid");
-    }
-    const searchResult = await Search_to_display_ticker(ticker as string); // Call the Search_ticker function
-    res.send(searchResult);
-} catch (error) {
-    console.error("Error searching ticker:", error);
-    res.status(500).send("Failed to search ticker");
-}
-});
-// Grouped Daily
-router.get("/grouped-daily", async (req: Request, res: Response) => {
-console.log("Getting grouped daily");
-try {
-    console.log("Calling Grouped Daily");
-    const response = await Grouped_Daily();
-    res.send(response);
-} catch (error) {
-    console.log(error);
-    res.status(500).send("Failed to get data");
-}
-});
+
 
 // Latest News 
 router.get("/news", async (req: Request, res: Response) => {
@@ -117,7 +50,7 @@ try {
 }
 });
 
-
+// top 5 increased in price stock
 router.get("/top5", async (req: Request, res: Response) => {
   try {
     console.log("Calling Top 5");
@@ -128,6 +61,8 @@ router.get("/top5", async (req: Request, res: Response) => {
     res.status(500).send("Failed to get Top 5");
 }
 });
+
+// bottom 5 decreased in price stock
 router.get("/losers", async (req: Request, res: Response) => {
   try {
     console.log("Calling Biggests Losers ");
@@ -137,21 +72,6 @@ router.get("/losers", async (req: Request, res: Response) => {
     console.log(error);
     res.status(500).send("Failed to get Losers");
 }
-});
-
-// daily historical data throughout the day
-router.get("/daily-historical", async (req: Request, res: Response) => {
-  try {
-    const { ticker } = req.query; // Get the ticker parameter from the request query
-    if (!ticker || typeof ticker !== "string") {
-        throw new Error("Ticker parameter is missing or invalid");
-    }
-    const searchResult = await Historical_Daily_By_Ticker(ticker as string);
-    res.send(searchResult);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Failed to get daily historical");
-  }
 });
 
 // Stock profile
@@ -171,38 +91,6 @@ router.get("/financial-summary", async (req: Request, res: Response) => {
 
 
 
-
-
-// Not implemented Yet
-router.get("/qoute", async (req: Request, res: Response) => {
-  try {
-    const { ticker } = req.query; // Get the ticker parameter from the request query
-    if (!ticker || typeof ticker !== "string") {
-      throw new Error("Ticker parameter is missing or invalid");
-    }
-    const searchResult = await Single_Stock_Qoute(ticker as string); // Call the Search_ticker function
-    res.send(searchResult);
-  } catch (error) {
-    console.error("Error searching ticker:", error);
-    res.status(500).send("Failed to search ticker for Qoute");
-  }
-});
-
-// Not implemented Yet
-// interval data throughout the day
-router.get("/intraday", async (req: Request, res: Response) => {
-  try {
-    const { ticker } = req.query; // Get the ticker parameter from the request query
-    if (!ticker || typeof ticker !== "string") {
-        throw new Error("Ticker parameter is missing or invalid");
-    }
-    const searchResult = await IntraDay_Given_Stock_and_Timeframe(ticker as string);
-    res.send(searchResult);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Failed to get IntraData");
-  }
-});
 
 export default router;
 
--- backend-fct/src/data-fetching/fetch-stocks-data.ts
+++ backend-fct/src/data-fetching/fetch-stocks-data.ts
@@ -1,21 +1,4 @@
 
-export default async function daily_AvgPrice() {
-  const API_KEY = process.env.API_KEY;
-  const request = await fetch(
-    "https://api.twelvedata.com/avgprice?apikey=0cedfa55583c4f2da1f7985dd27c48b1&interval=1day&symbol=NVDA&country=US&format=JSON"
-  );
-  const response = await request.json();
-  return response;
-}
-
-export async function Exponential_Moving_Avg() {
-  const API_KEY = process.env.Poly_API_KEY;
-  const request = await fetch(
-    "https://api.polygon.io/v1/indicators/ema/AAPL?timespan=day&adjusted=true&window=10&series_type=close&order=desc&limit=500&apiKey=UyzPkn5wTGhDq7aauKltPyTyNburS6FC"
-  );
-  const response = await request.json();
-  return response;
-}
 // searching with Company/ticker name
 export async function Search_ticker(ticker: string) {
   const API_KEY = "UyzPkn5wTGhDq7aauKltPyTyNburS6FC";
@@ -25,41 +8,7 @@ export async function Search_ticker(ticker: string) {
   const response = await request.json();
   return response;
 }
-export async function Search_to_display_ticker(ticker: string) {
-  try {
-      const API_KEY = process.env.Poly_API_KEY;
-      const request = await fetch(
-          `https://api.polygon.io/v1/indicators/sma/${ticker}?timespan=day&adjusted=true&window=7&series_type=close&order=asc&limit=100&apiKey=${API_KEY}`
-      );
-      if (!request.ok) {
-          throw new Error(`Failed to fetch data for ${ticker}. Status: ${request.status}`);
-      }
-      const response = await request.json();
-      return response;
-  } catch (error) {
-      console.error("Error fetching ticker data:", error);
-      throw error; // Re-throw the error to be handled by the caller
-  }
-}
-// Group Daily
-export async function Grouped_Daily() {
-  try {
-    const API = process.env.Poly_API_KEY;
-    const request = await fetch(
-      `https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-01-09?adjusted=true&apiKey=UyzPkn5wTGhDq7aauKltPyTyNburS6FC`
-    );
-    if (!request.ok) {
-      throw new Error(
-        `Failed to fetch data , Status: ${request.status}`
-      );
-    }
-    const response = await request.json();
-    return response;
-  } catch (error) {
-    console.error("Error fetching Grouped Daily data:", error);
-    throw error; // Re-throw the error to be handled by the caller
-  }
-}
+
 
 // Latest News 
 export async function News_latest() {
@@ -71,47 +20,6 @@ export async function News_latest() {
     if (!request.ok) {
       throw new Error(
         `Failed to fetch data , Status: ${request.status}`
-      );
-    }
-    const response = await request.json();
-    return response;
-  } catch (error) {
-    console.error("Error fetching Grouped Daily data:", error);
-    throw error; // Re-throw the error to be handled by the caller
-  }
-}
-
-// Full Qoute Single Stock (to be implemented)
-export async function Single_Stock_Qoute(ticker: string) {
-  try {
-    const request = await fetch(
-      `https://financialmodelingprep.com/api/v3/quote/${ticker}apikey=wc2bbHWhFBL7no45kaUlx2xLHI2z2wv1`
-    );
-    if (!request.ok) {
-      throw new Error(
-        `Failed to fetch data for ${ticker} Qoute From FMP, Status: ${request.status}`
-      );
-    }
-    const response = await request.json();
-    return response;
-  } catch (error) {
-    console.error("Error fetching Grouped Daily data:", error);
-    throw error; // Re-throw the error to be handled by the caller
-  }
-}
-//financialmodelingprep.com/api/v3/historical-chart/15min/AAPL?from=2024-02-21&to=2024-02-21&apikey=wc2bbHWhFBL7no45kaUlx2xLHI2z2wv1
-
-
-// Not implemented Yet
-// Stock data every 15min intervals in a day
-export async function IntraDay_Given_Stock_and_Timeframe(ticker: string) {
-  try {
-    const request = await fetch(
-      `https://financialmodelingprep.com/api/v3/historical-chart/15min/${ticker}?from=2024-02-21&to=2024-02-21&apikey=wc2bbHWhFBL7no45kaUlx2xLHI2z2wv1`
-    );
-    if (!request.ok) {
-      throw new Error(
-        `Failed to fetch Daily data for ${ticker} Qoute From FMP, Status: ${request.status}`
       );
     }
     const response = await request.json();
@@ -169,7 +77,7 @@ export async function Losers() {
 }
 
 // Full historical daily data for last 5 years
-export async function Historical_Daily_By_Ticker(ticker: string) {
+export default async function Historical_Daily_By_Ticker(ticker: string) {
   try {
     const request = await fetch(
       `https://financialmodelingprep.com/api/v3/historical-price-full/${ticker}?apikey=wc2bbHWhFBL7no45kaUlx2xLHI2z2wv1`
@@ -186,7 +94,7 @@ export async function Historical_Daily_By_Ticker(ticker: string) {
     throw error; // Re-throw the error to be handled by the caller
   }
 }
-// Full historical daily data for last 5 years
+// Stock Company Profile
 export async function Stock_Company_Profile(ticker: string) {
   try {
     const request = await fetch(
