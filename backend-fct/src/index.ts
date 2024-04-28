54cc8d6d6f9d8bda1c7361211b877e8f313c20fa
ma942 <ma942@student.le.ac.uk>
2024-04-29 00:48:52 +0100

    port status changed

--- backend-fct/src/index.ts
+++ backend-fct/src/index.ts
@@ -6,7 +6,7 @@ import router from "../routes/router";
 // dotenv.config();
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 
 // cors access
