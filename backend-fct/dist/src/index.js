2e3200658190f5fb9bdb3de68a1497b12ce1eb41
ma942 <ma942@student.le.ac.uk>
2024-04-29 23:22:15 +0100

    backedn deployed on google app engine

--- backend-fct/dist/src/index.js
+++ backend-fct/dist/src/index.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const router_1 = __importDefault(require("../routes/router"));
 // dotenv.config();
 const app = (0, express_1.default)();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 // cors access
 app.use(cors({
